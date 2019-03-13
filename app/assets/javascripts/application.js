c51ac82b63f9f11df06dfb5d7237cea414398df0
ajit123jain <ajitkumarjain123@gmail.com>
2019-03-13 10:29:09 +0530

    text color

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,6 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery
-//= require jquery.miniColors.min
+//= require jquery.minicolors
 //= require bootstrap.min
 //= require_tree .
